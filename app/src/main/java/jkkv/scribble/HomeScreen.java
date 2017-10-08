f7bc011a6535d941adc37b65da9cc2012dc437a3
Joe <hoangjd@dukes.jmu.edu>
2017-10-08 15:11:53 -0400

    functionality to the New Project button

--- app/src/main/java/jkkv/scribble/HomeScreen.java
+++ app/src/main/java/jkkv/scribble/HomeScreen.java
@@ -2,14 +2,29 @@ package jkkv.scribble;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class HomeScreen extends Activity {
+    Button newProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        canvasAct();
     }
 
+    public void canvasAct(){
+            newProject = (Button)findViewById(R.id.newProject);
+            newProject.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent canvasAct = new Intent(HomeScreen.this,Canvas.class);
+                    startActivity(canvasAct);
+                }
+            });
+    }
 
 }
