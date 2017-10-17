c0d64e4a64a2ff49148acacb6259ebbff310802e
Joe <hoangjd@vcu.edu>
2017-10-17 13:34:39 -0400

    floating action button functionality

--- app/src/main/java/jkkv/scribble/Canvas.java
+++ app/src/main/java/jkkv/scribble/Canvas.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class Canvas extends AppCompatActivity {
+
+    public FloatingActionButton menu2, draw2;
+    public Animation open, close;
+    public Boolean isOpen = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +24,36 @@ public class Canvas extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        draw2 = (FloatingActionButton) findViewById(R.id.draw);//make action buttons for draw and menu
+        menu2 = (FloatingActionButton) findViewById(R.id.menu);
+        open = AnimationUtils.loadAnimation(this, R.anim.butfunction);//wiring to anim folder
+        close = AnimationUtils.loadAnimation(this, R.anim.butfunctionclose);
+
+
+
+        menu2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                animateMenu();
             }
         });
+
+
+        //include onclick listeners once we have functionality to each of menu options
+    }
+
+
+    public void animateMenu() {//says whether or not to open menu
+        if (isOpen){
+            draw2.startAnimation(close);
+            draw2.setClickable(false);
+            isOpen = false;
+        }
+        else
+            draw2.startAnimation(open);
+            draw2.setClickable(true);
+            isOpen = true;
+
     }
 
 }
