22dfe860af43cf4126a12f12316acb44c39ab9f2
Joe <hoangjd@vcu.edu>
2017-10-16 21:22:35 -0400

    Fixed Initial Issues

--- app/src/main/java/jkkv/scribble/HomeScreen.java
+++ app/src/main/java/jkkv/scribble/HomeScreen.java
@@ -1,31 +1,31 @@
 package jkkv.scribble;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 
-public class HomeScreen extends Activity {
+public class HomeScreen extends AppCompatActivity {
     Button newProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        canvasAct();
-        loadAct();
-    }
+    canvasAct();
+    loadAct();
+}
 
     public void canvasAct(){
-            newProject = (Button)findViewById(R.id.newProject);
-            newProject.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    Intent canvasAct = new Intent(HomeScreen.this,Canvas.class);
-                    startActivity(canvasAct);
-                }
-            });
+        newProject = (Button)findViewById(R.id.newProject);
+        newProject.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent canvasAct = new Intent(HomeScreen.this,Canvas.class);
+                startActivity(canvasAct);
+            }
+        });
     }
 
     public void loadAct(){
@@ -33,10 +33,9 @@ public class HomeScreen extends Activity {
         newProject.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent loadAct = new Intent(HomeScreen.this,LoadExisiting.class);
+                Intent loadAct = new Intent(HomeScreen.this,LoadExisting.class);
                 startActivity(loadAct);
             }
         });
     }
-
 }
--- app/src/main/java/jkkv/scribble/LoadExisting.java
+++ app/src/main/java/jkkv/scribble/LoadExisting.java
@@ -1,13 +1,13 @@
 package jkkv.scribble;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoadExisiting extends Activity {
+public class LoadExisting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_exisiting);
+        setContentView(R.layout.activity_load_existing);
     }
 }
