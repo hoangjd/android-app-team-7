f13f9b2862335672f5668c59c44395d4b519aec6
Joe <hoangjd@vcu.edu>
2017-10-18 23:18:26 -0400

    Updated espresso ExitingMenuTest

--- app/src/androidTest/java/jkkv/scribble/ExitingMenuTest.java
+++ app/src/androidTest/java/jkkv/scribble/ExitingMenuTest.java
@@ -13,7 +13,10 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
+
 /**
  * Created by Keri on 10/18/2017.
  */
@@ -29,9 +32,49 @@ public class ExitingMenuTest {
                 .check(matches(isClickable()));
         onView(withId(R.id.menu))
                 .perform(click());
+        onView(withId(R.id.draw))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.eraser))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.home))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.paintbrush))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.movinghand))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.background))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.port))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.color))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.reflection))
+                .check(matches(isDisplayed()));
         onView(withId(R.id.menu))
                 .check(matches(isClickable()));
+
+
         onView(withId(R.id.menu))
                 .perform(click());
+        onView(withId(R.id.draw))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.eraser))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.home))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.paintbrush))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.movinghand))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.background))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.port))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.color))
+                .check(matches(not(isDisplayed())));
+        onView(withId(R.id.reflection))
+                .check(matches(not(isDisplayed())));
     }
+
+
 }
