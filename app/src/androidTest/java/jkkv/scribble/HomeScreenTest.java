51689fea6a2f5456b53ab1158a720e06b3451932
Keri Chamberlain <chamberlainks@vcu.edu>
2017-10-19 14:38:31 -0400

    Merge branch 'master' of C:\Users\Keri\android-app-team-7 with conflicts.

--- app/src/androidTest/java/jkkv/scribble/HomeScreenTest.java
+++ app/src/androidTest/java/jkkv/scribble/HomeScreenTest.java
@@ -31,6 +31,7 @@ public class HomeScreenTest {
     private HomeScreen homeScreen = null;
 
     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(Canvas.class.getName(),null,false);
+    Instrumentation.ActivityMonitor monitor2 = getInstrumentation().addMonitor(Canvas.class.getName(),null,false);
 
     @Before
     public void setUp() throws Exception {
@@ -76,11 +77,196 @@ public class HomeScreenTest {
         assertNotNull(homeScreen.findViewById(R.id.newProject));
     }
 
+    //Canvas: Senario 1
+    @Test
+    public void testLaunchOfCanvasOnButtonClick() {
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        canvas.finish();
+
+    }
+
+    //Canvas: Senario 2
+    @Test
+    public void testGoingBackToCanvasAfterMenu() {
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        assertNotNull(canvas.findViewById(R.id.menu));
+
+        onView(withId(R.id.draw)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.eraser)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.home)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.paintbrush)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.movinghand)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.background)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.port)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.color)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.reflection)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.menu)).perform(click());
+
+        onView(withId(R.id.draw)).check(matches(isDisplayed()));
+        onView(withId(R.id.eraser)).check(matches(isDisplayed()));
+        onView(withId(R.id.home)).check(matches(isDisplayed()));
+        onView(withId(R.id.paintbrush)).check(matches(isDisplayed()));
+        onView(withId(R.id.movinghand)).check(matches(isDisplayed()));
+        onView(withId(R.id.background)).check(matches(isDisplayed()));
+        onView(withId(R.id.port)).check(matches(isDisplayed()));
+        onView(withId(R.id.color)).check(matches(isDisplayed()));
+        onView(withId(R.id.reflection)).check(matches(isDisplayed()));
+
+
+    }
+
+    //Canvas: Senario 3
+    @Test
+    public void testLaunchOfCanvasAfterExitBlank() {
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        canvas.finish();
+
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas2 = getInstrumentation().waitForMonitorWithTimeout(monitor2,5000);
+
+        assertNotNull(canvas2);
+
+    }
+
+    //Menu: Senario 1
+    @Test
+    public void testLaunchOfMenuFromCanvas() {
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        assertNotNull(canvas.findViewById(R.id.menu));
+
+        onView(withId(R.id.draw)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.eraser)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.home)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.paintbrush)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.movinghand)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.background)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.port)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.color)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.reflection)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.menu)).perform(click());
+
+        onView(withId(R.id.draw)).check(matches(isDisplayed()));
+        onView(withId(R.id.eraser)).check(matches(isDisplayed()));
+        onView(withId(R.id.home)).check(matches(isDisplayed()));
+        onView(withId(R.id.paintbrush)).check(matches(isDisplayed()));
+        onView(withId(R.id.movinghand)).check(matches(isDisplayed()));
+        onView(withId(R.id.background)).check(matches(isDisplayed()));
+        onView(withId(R.id.port)).check(matches(isDisplayed()));
+        onView(withId(R.id.color)).check(matches(isDisplayed()));
+        onView(withId(R.id.reflection)).check(matches(isDisplayed()));
+    }
+
+    //Menu: Senario 2
+    @Test
+    public void testClosingMenu(){
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        onView(withId(R.id.menu)).perform(click());
+
+        onView(withId(R.id.draw)).check(matches(isDisplayed()));
+        onView(withId(R.id.eraser)).check(matches(isDisplayed()));
+        onView(withId(R.id.home)).check(matches(isDisplayed()));
+        onView(withId(R.id.paintbrush)).check(matches(isDisplayed()));
+        onView(withId(R.id.movinghand)).check(matches(isDisplayed()));
+        onView(withId(R.id.background)).check(matches(isDisplayed()));
+        onView(withId(R.id.port)).check(matches(isDisplayed()));
+        onView(withId(R.id.color)).check(matches(isDisplayed()));
+        onView(withId(R.id.reflection)).check(matches(isDisplayed()));
+        assertNotNull(canvas);
+
+        onView(withId(R.id.menu)).perform(click());
+
+        onView(withId(R.id.draw)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.eraser)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.home)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.paintbrush)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.movinghand)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.background)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.port)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.color)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.reflection)).check(matches(not(isDisplayed())));
+        assertNotNull(canvas);
+    }
+
+    //Menu: Senario 3
+    @Test
+    public void testLaunchOfMenuFromHome() {
+        assertNotNull(homeScreen.findViewById(R.id.newProject));
+
+        onView(withId(R.id.newProject)).perform(click());
+
+        Activity canvas = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+
+        assertNotNull(canvas);
+
+        assertNotNull(canvas.findViewById(R.id.menu));
+
+        onView(withId(R.id.draw)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.eraser)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.home)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.paintbrush)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.movinghand)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.background)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.port)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.color)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.reflection)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.menu)).perform(click());
+
+        onView(withId(R.id.draw)).check(matches(isDisplayed()));
+        onView(withId(R.id.eraser)).check(matches(isDisplayed()));
+        onView(withId(R.id.home)).check(matches(isDisplayed()));
+        onView(withId(R.id.paintbrush)).check(matches(isDisplayed()));
+        onView(withId(R.id.movinghand)).check(matches(isDisplayed()));
+        onView(withId(R.id.background)).check(matches(isDisplayed()));
+        onView(withId(R.id.port)).check(matches(isDisplayed()));
+        onView(withId(R.id.color)).check(matches(isDisplayed()));
+        onView(withId(R.id.reflection)).check(matches(isDisplayed()));
+    }
+
     @After
     public void tearDown() throws Exception {
         homeScreen = null;
     }
 
 }
-
 
