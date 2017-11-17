425d520985d501404ac9b635b0defea42c01d74c
Keri Chamberlain <chamberlainks@vcu.edu>
2017-11-16 22:17:11 -0500

    Merge branch 'master' of C:\Users\Keri\android-app-team-7 with conflicts.

---
+++ app/src/test/java/jkkv/scribble/ColorPaletteUnitTests.java
@@ -0,0 +1,16 @@
+package jkkv.scribble;
+import org.junit.Test;
+import static org.mockito.mock.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Keri on 11/16/2017.
+ */
+
+public class ColorPaletteUnitTests {
+    @Test
+    public void testColorPaletteScenario1 () throws Exception
+    {
+
+    }
+}
