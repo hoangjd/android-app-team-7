7b05722b400f5d5049014688f7d3600545520ea4
Keri Chamberlain <chamberlainks@vcu.edu>
2017-11-16 23:20:25 -0500

    mockito in gradle

--- app/src/test/java/jkkv/scribble/ColorPaletteUnitTests.java
+++ app/src/test/java/jkkv/scribble/ColorPaletteUnitTests.java
@@ -1,15 +1,23 @@
 package jkkv.scribble;
+import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.mock.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.internal.impl.PowerMockJUnit44RunnerDelegateImpl;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Keri on 11/16/2017.
  */
-
+@RunWith(PowerMockRunner.class)
 public class ColorPaletteUnitTests {
+
+
     @Test
-    public void testColorPaletteScenario1 () throws Exception
+    public void testColorPaletteScenario1() throws Exception
     {
 
     }
